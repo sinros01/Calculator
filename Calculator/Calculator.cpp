0914360f769271e58449a59c08dba94b5f54154f
sinros01 <69473399+sinros01@users.noreply.github.com>
2020-09-06 17:23:26 +0200

    added simple equation

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -3,27 +3,69 @@
 int num1, num2;
 char op;
 int answer;
+int operation;
 
 int main() {
-	std::cout << "Enter first number:";
-	std::cin >> num1;
-	std::cout << "Enter the operator:";
-	std::cin >> op;
-	std::cout << "Enter second number:";
-	std::cin >> num2;
+	std::cout << "Do you want to calculate basic or equation?\n";
+	std::cout << "For basic type 1\n";
+	std::cout << "For equation type 2\n";
+	std::cin >> operation;
 
-	if (op == '+'){
-		answer = num1 + num2;
-	}	else if (op == '-') {
+	if (operation == 1) {
+
+		std::cout << "Enter first number:";
+		std::cin >> num1;
+		std::cout << "Enter the operator:";
+		std::cin >> op;
+		std::cout << "Enter second number:";
+		std::cin >> num2;
+
+		if (op == '+') {
+			answer = num1 + num2;
+		}
+		else if (op == '-') {
 			answer = num1 - num2;
-	}	else if (op == '/') {
+		}
+		else if (op == '/') {
 			answer = num1 / num2;
-	}	else if (op == '*') {
+		}
+		else if (op == '*') {
 			answer = num1 * num2;
-	}		else {
-				std::cout << "invalid operator";
-	}
-	std::cout << answer;
+		}
+		else {
+			std::cout << "invalid operator";
+		}
+		std::cout << answer;
 
+	}
+	else if (operation == 2) {
+		std::cout << "Format will be:(first number)(the operator)X= (answer)\n";
+		std::cout << "Enter first number:";
+		std::cin >> num1;
+		std::cout << "Enter the operator:";
+		std::cin >> op;
+		std::cout << "Enter the answer;";
+		std::cin >> num2;
+
+		if (op == '+') {
+			answer = num2 - num1;
+		}
+		else if (op == '-') {
+			answer = num2 + num1;
+		}
+		else if (op == '/') {
+			answer = num1 * num2;
+		}
+		else if (op == '*') {
+			answer = num2 / num1;
+		}
+		else {
+			std::cout << "invalid operator";
+
+		
+		}
+		std::cout << answer;
+		
+	}
 	return 0;
 }
